307896bd77d74b29aef08ab49be8648b12ca24f4
maheshlamichhane048@gmail.com <maheshlamichhane048@gmail.com>
2025-09-11 15:51:00 +0545

    lvti features completed

---
+++ src/main/java/com/lamichhane/lvti/LVTIMain.java
@@ -0,0 +1,13 @@
+package com.lamichhane.lvti;
+
+public class LVTIMain {
+
+    public static void main(String[] args) {
+        var x = 2;
+        var y = 3.2;
+        var t = "hello";
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(t);
+    }
+}
